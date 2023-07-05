7df4b8dcd875ba4966b45651832297ec3c246314
Ksenia Mandryka <ksenia_mandryka@mail.ru>
2023-07-05 20:40:13 +0300

    some fixes after the checker

--- test/unit/form.test.jsx
+++ test/unit/form.test.jsx
@@ -18,28 +18,36 @@ describe("Форма отправки", () => {
     await addItemToCart({ id: 0, history, count: 1 })
     history.push("/cart")
     const checkoutBtn = container.querySelector(".Form-Submit")
-    await events.click(checkoutBtn)
-    const errorMsg = container.querySelector(".invalid-feedback")
-    expect(errorMsg).toBeInTheDocument()
+
+    if(checkoutBtn) {
+      await events.click(checkoutBtn)
+      const errorMsg = container.querySelector(".invalid-feedback")
+      expect(errorMsg).toBeInTheDocument()
+    }
   })
 
+  
+  
+  
   it("Форма с валидными данными отправляется", async () => {
     const { container, history, queryByRole } = wrapWithProviders()
     await addItemToCart({ id: 0, history, count: 1 })
     history.push("/cart")
-
+  
     const nameField = queryByRole("textbox", { name: "Name" })
     const phoneField = queryByRole("textbox", { name: "Phone" })
     const addressField = queryByRole("textbox", { name: "Address" })
 
-    await events.type(nameField, params.name)
-    await events.type(phoneField, params.phone)
-    await events.type(addressField, params.address)
+    if(nameField && phoneField && addressField) {
+      await events.type(nameField, params.name)
+      await events.type(phoneField, params.phone)
+      await events.type(addressField, params.address)
+      const checkoutBtn = container.querySelector(".Form-Submit")
+      await events.click(checkoutBtn)
+      const successMsg = document.querySelector(".Cart-SuccessMessage")
+      await waitFor(() => expect(successMsg).toBeInTheDocument())
+    }
 
-    const checkoutBtn = container.querySelector(".Form-Submit")
-    await events.click(checkoutBtn)
-    const successMsg = document.querySelector(".Cart-SuccessMessage")
-    await waitFor(() => expect(successMsg).toBeInTheDocument())
   })
 
   test("сообщение об сделанном заказе", async () => {
@@ -47,24 +55,26 @@ describe("Форма отправки", () => {
     await addItemToCart({ id: 0, history, count: 1 })
     history.push("/cart")
 
-    // const params = {
-    //   name: "Иван",
-    //   phone: "01234567891",
-    //   address: "Москва"
-    // }
+    const params = {
+      name: "Иван",
+      phone: "01234567891",
+      address: "Москва"
+    }
 
     const nameField = queryByRole("textbox", { name: "Name" })
     const phoneField = queryByRole("textbox", { name: "Phone" })
     const addressField = queryByRole("textbox", { name: "Address" })
+    if(nameField && phoneField && addressField) {
+      await events?.type(nameField, params.name)
+      await events?.type(phoneField, params.phone)
+      await events?.type(addressField, params.address)
+      const checkoutBtn = container.querySelector(".Form-Submit")
+      await events.click(checkoutBtn)
+      const successMsg = document.querySelector(".alert-success")
+      await waitFor(() => expect(successMsg)?.toBeInTheDocument())
+    }
 
-    await events.type(nameField, params.name)
-    await events.type(phoneField, params.phone)
-    await events.type(addressField, params.address)
 
-    const checkoutBtn = container.querySelector(".Form-Submit")
-    await events.click(checkoutBtn)
-    const successMsg = document.querySelector(".alert-success")
-    await waitFor(() => expect(successMsg).toBeInTheDocument())
   })
 
 })
